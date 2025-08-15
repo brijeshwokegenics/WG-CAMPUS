a23d6c50e17f3f99755edd2d5c205e61405b305a
WokeGenics <wokegenics@gmail.com>
2025-08-15 10:06:41 +0000

    delete complete project

--- src/components/logo.tsx
+++
@@ -1,9 +0,0 @@
-import { GraduationCap } from "lucide-react";
-
-export function Logo() {
-  return (
-    <div className="flex items-center justify-center bg-primary text-primary-foreground w-16 h-16 rounded-full">
-      <GraduationCap className="h-8 w-8" />
-    </div>
-  );
-}
