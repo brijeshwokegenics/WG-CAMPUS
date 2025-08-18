3f78002213639635458d9e37d439b349c31869f3
WokeGenics <wokegenics@gmail.com>
2025-08-18 05:36:25 +0000

    continue

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -4,7 +4,7 @@
 import React, { useState, useMemo } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Menu, School, ChevronDown, GraduationCap, Briefcase, Building, MessageSquare, PersonStanding, BookUser, LayoutDashboard, Users, UserCog, Printer, ClipboardList, Calendar, FileText, Book, Wallet, Banknote, Warehouse, Bus, Library, Hotel, Ticket, Info, Webhook } from "lucide-react";
+import { Menu, School, ChevronDown, GraduationCap, Briefcase, Building, MessageSquare, PersonStanding, BookUser, LayoutDashboard, Users, UserCog, Printer, ClipboardList, Calendar, FileText, Book, Wallet, Banknote, Warehouse, Bus, Library, Hotel, Ticket, Info, Webhook, UserCheck } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
