e047c106baaa30f27091bd487cca546a75a1e55f
WokeGenics <wokegenics@gmail.com>
2025-08-21 08:23:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/director/dashboard/[id]/parent/dashboard/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is obsolete and will be removed.
-// The new parent dashboard is at /app/parent/[schoolId]/dashboard/page.tsx
--- src/app/director/dashboard/[id]/parent/profile/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is obsolete and will be removed.
-// The new parent profile page is at /app/parent/[schoolId]/profile/page.tsx
